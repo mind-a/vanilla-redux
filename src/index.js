9e012c43f2224e1f6b66421eafad3c1a30063675
dahyeMin <sol1829@gmail.com>
2021-02-09 21:13:58 +0900

    #1.2 Action

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-    return state;
+const countModifier = (count = 0, action) => {
+   // console.log(action);
+    //처음 call할때 initialize
+    if(action.type === "ADD"){
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 //reducer는 data를 modify하는 function임
 //countModifier가 state를 수정함
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+
 console.log(countStore.getState());
