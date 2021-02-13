6e03e4f99d399d4810e52bc7ca2fb48d33fbe2fd
dahyeMin <sol1829@gmail.com>
2021-02-13 16:30:01 +0900

    #2.1 State Mutation

--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 const reducer = (state=[], action) => {
-    console.log(action);
     switch (action.type) {
         case ADD_TODO:
-            return [];
+            return [...state, {text: action.text, id: Date.now() }];
         case DELETE_TODO:
             return [];
+            //...은 ES6 spread
+            //means : 모든 state array의 contents
         default:
             return state;
-    }
     //절대 mutate state쓰지 말 것!
-}
+    //mutation 변형하는것이 아니라 새로운 상태를 return 해야한다..!
+    //state push같은 것들 X
+    }
+};
 
 const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()))
 
 const onSubmit = e => {
     e.preventDefault();
